2b69c44bad35b127763555856a7f5ae528637339
Chris Williams <17027446+mistwire@users.noreply.github.com>
2024-08-10 20:33:47 -0400

    up to exercise 59

---
+++ 057-exercise/main.go
@@ -0,0 +1,16 @@
+// Using the code you wrote in the previous hands-on exercise ( -o to customize name of build):
+// ● build your program for windows
+// 	○ GOOS=windows GOARCH=amd64 go build
+// ● build your program for mac (apple silicon)
+// 	○ GOOS=darwin GOARCH=arm64 go build -o myapp-macos-arm64
+// ● build your program for linux
+// 	○ GOOS=linux GOARCH=amd64 go build -o myapp-linux
+
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Will code for 🍕")
+}
