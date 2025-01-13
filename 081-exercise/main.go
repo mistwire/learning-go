06731a50380b60db006a49611f0be64b7ad8b0e7
Chris Williams <17027446+mistwire@users.noreply.github.com>
2025-01-13 17:25:58 -0500

    starting over

--- 081-exercise/main.go
+++ 081-exercise/main.go
@@ -6,13 +6,14 @@ import (
 )
 
 func main() {
-	for i := 0; i < 100; i++ {
-		fmt.Println(i)
-	}
+	// for i := 0; i < 100; i++ {
+	// 	fmt.Println(i)
+	// }
 
 	for i := 0; i < 100; i++ {
 		x := rand.Intn(10)
 		y := rand.Intn(10)
+		fmt.Printf("Iteration %v:\n", i)
 		fmt.Printf("X is %v\tY is %v\t", x, y)
 		switch {
 		case x < 4 && y < 4:
