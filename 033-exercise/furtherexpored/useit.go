9508c40eac2f46a2fabae1392402430a434d6f8b
Chris Williams <17027446+mistwire@users.noreply.github.com>
2025-01-14 21:32:43 -0500

    exercise 33

---
+++ 033-exercise/furtherexpored/useit.go
@@ -0,0 +1,12 @@
+package furtherexplored
+
+import "fmt"
+
+// this is also "package block" scope
+// this is exported because the identifier "Fasincating" is capitalized
+func Fascinating() {
+	fmt.Println("Planet speed:", planetSpeed)
+
+	planetRotationSpeed := 1000
+	fmt.Println("Planet spinning speed:", planetRotationSpeed)
+}
---
+++ 033-exercise/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"learning-go/033-exercise/furtherexplored"
+)
+
+// x is in "package block" scope
+var x = 42
+
+func main() {
+	// x can be accessed here
+	fmt.Println(x)
+
+	// x can be accessed here
+	printMe()
+
+	// y does not exist here yet
+	// so this won't work
+	// fmt.Println(y)
+
+	// y is in "block scope"
+	y := 43
+	fmt.Println(y)
+
+	p1 := person{
+		"James",
+	}
+	p1.sayHello()
+
+	// variable "shadowing"
+	x := 32
+	fmt.Println(x)
+
+	// package block x is still the same
+	printMe()
+
+	furtherexplored.Fascinating()
+
+	//also good to know
+
+	if z := 82; z > 50 {
+		fmt.Println(z)
+	}
+	// you can't access z here
+	// fmt.Println(z)
+	/*
+		take a look at the "comma ok idiom"
+		https://go.dev/doc/effective_go#maps
+	*/
+}
+
+func printMe() {
+	//x can be accessed here
+	fmt.Println(x)
+}
+
+// type person is in "package block" scope
+type person struct {
+	first string
+}
+
+// the method sayHello
+// which is attached to VALUES of TYPE person
+// is in "package block" scope
+func (p person) sayHello() {
+	fmt.Println("Hi, my name is", p.first)
+}
